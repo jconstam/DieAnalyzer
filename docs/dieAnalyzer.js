1907e545ba5714f6d2f493b4f8dd6b5929614545
Jason Constam <callmebob@gmail.com>
2018-10-28 16:21:49 +0100

    Added chi-squared test

--- docs/dieAnalyzer.js
+++ docs/dieAnalyzer.js
@@ -2,26 +2,26 @@ String.prototype.lpad = function( padString, length )
 {
 	var str = this;
     while ( str.length < length )
-        str = padString + str;
-    return str;
+        str = padString + str
+    return str
 }
 
 function getLatestRollSanitized( )
 {
-	var input = document.getElementById( "latestRoll" ).value;
+	var input = document.getElementById( "latestRoll" ).value
 	if( isNaN( input ) )
 	{
-		alert( input + " is not a number" );
-		return null;
+		alert( input + " is not a number" )
+		return null
 	}
 	else if( input < 1 || input > 20 )
 	{
-		alert( input + " is out of range" );
-		return null;
+		alert( input + " is out of range" )
+		return null
 	}
 	else
 	{
-		return input;
+		return input
 	}
 }
 
@@ -39,7 +39,12 @@ function getDivWithNumberValue( divName )
 function setDivWithPercent( divName, percent )
 {
 	var pct = percent * 100
-	document.getElementById( divName ).innerHTML = pct.toFixed( 0 ) + "%"
+	document.getElementById( divName ).innerHTML = pct.toFixed( 1 ) + "%"
+}
+
+function setDiv( divName, number )
+{
+	document.getElementById( divName ).innerHTML = number
 }
 
 function updateResults( newValue )
@@ -48,16 +53,38 @@ function updateResults( newValue )
 	incrementDivWithNumber( "results" + newValue.toString( ).lpad( "0", 2 ) )
 	
 	var totalRolls = getDivWithNumberValue( "totalRolls" )
+	var expected = totalRolls / 20
+	var chiSquared = 0
 	for( face = 1; face <= 20; face++ )
 	{
-		var faceValue = getDivWithNumberValue( "results" + face.toString( ).lpad( "0", 2 ) )
-		setDivWithPercent( "resultPct" + face.toString( ).lpad( "0", 2 ), faceValue / totalRolls )
+		var faceCount = getDivWithNumberValue( "results" + face.toString( ).lpad( "0", 2 ) )
+		setDivWithPercent( "resultPct" + face.toString( ).lpad( "0", 2 ), faceCount / totalRolls )
+		
+		chiSquared += Math.pow( faceCount - expected, 2 ) / expected
+	}
+	
+	setDiv( "chiSquared", chiSquared.toFixed( 2 ) )
+	if( totalRolls < 400 )
+	{
+		setDiv( "status", "Not enough samples" )
+	}
+	else if( chiSquared < 27 )
+	{
+		setDiv( "status", "Not biased" )
+	}
+	else if( chiSquared < 30 )
+	{
+		setDiv( "status", "Possibly biased" )
+	}
+	else
+	{
+		setDiv( "status", "Likely biased" )
 	}
 }
 
 function addNewRoll( )
 {
-	var input = getLatestRollSanitized( );	
+	var input = getLatestRollSanitized( )
 	if( input == null )
 	{
 		return;
@@ -68,5 +95,8 @@ function addNewRoll( )
 
 function addRandomRoll( )
 {
-	updateResults( Math.floor( ( Math.random( ) * 20 ) + 1 ) )
+	for( i = 0; i < 400; i++ )
+	{
+		updateResults( Math.floor( ( Math.random( ) * 20 ) + 1 ) )
+	}
 }
